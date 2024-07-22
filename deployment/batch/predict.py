639afeea4637c711ca2ea5b676c5264faa3fcebb
Tiamz <t.sukanmi@gmail.com>
2024-07-22 17:47:55 +0000

    updating as a web service

--- deployment/batch/predict.py
+++ deployment/batch/predict.py
@@ -2,37 +2,49 @@ import pickle
 import os
 import pandas as pd
 import numpy as np
-import os
+import mlflow
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from flask import Flask
-import request, jsonify
+from flask import Flask, jsonify, request
 
+def get_and_clean_data(input_file, is_json=True):
+    if is_json:
+        # Convert JSON to DataFrame
+        df = pd.json_normalize(input_file)
+    else:
+        # Convert CSV to DataFrame
+        df = pd.read_csv(input_file)
 
-def get_and_clean_data(input_file):
-    data = pd.read_csv(input_file)
-    data.replace('#NUM!', np.nan, inplace=True)
-    data.dropna()
-    data = data.apply(pd.to_numeric, errors='coerce')
-    cleaned_data = data.dropna()
+    df.replace('#NUM!', np.nan, inplace=True)
+    df.dropna(inplace=True)
+    df = df.apply(pd.to_numeric, errors='coerce')
+    cleaned_data = df.dropna()
     return cleaned_data
 
-def feature_engineering(df):
+def feature_engineering(df, is_prediction=True):
     X = df.drop(columns=['is_safe'])
     y = df['is_safe']
+    
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
-    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.3, random_state=42)
-    return X_train, X_test, y_train, y_test
-
+    
+    if is_prediction:
+        # Return only the scaled features for prediction
+        return X_scaled
+    else:
+        # Perform train-test split if not just prediction
+        X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.3, random_state=42)
+        return X_train, X_test, y_train, y_test
 
 def load_model():
     try:
+        print('Loading model from GCS...')
         model_uri = "gs://water_quality_model/1/b148d239d6b84b08961892fe7b8dfc95/artifacts/model/gb_model"
         model = mlflow.sklearn.load_model(model_uri)
         print("Model loaded from GCS.")
     except Exception as e:
-        print(f"Error loading model from GCS: {e}")
+        print(f"Error loading model from GCS: {e}. Now loading from local binary file...")
+       
         with open('best_log_reg.bin', 'rb') as f_in:
             model = pickle.load(f_in)
             print("Model loaded from local binary file.")
@@ -40,47 +52,43 @@ def load_model():
 
 model = load_model()
 
-def apply_model(input_file, output_file):
-    df = get_and_clean_data(input_file)
-    
-    if 'is_safe' not in df.columns:
-        raise KeyError("'is_safe' column is missing from the data.")
-    
-    X_train, X_test, y_train, y_test = feature_engineering(df)
-    model = load_model()
-    y_pred = model.predict(X_test)
-    df_result = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
-    # df_result['model_version'] = run_id
-
-    output_dir = os.path.dirname(output_file)
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    df_result.to_csv(output_file, index=False)
-
-    return df_result
-
+app = Flask('Drinking-water-safety-prediction')
 
 @app.route('/predict', methods=['POST'])
 def predict_endpoint():
-    data = request.get_json()
-    df = get_and_clean_data(data)
-    
-    if 'is_safe' not in df.columns:
-        return jsonify({'error': "'is_safe' column is missing from the data."}), 400
-    
-    X = feature_engineering(df)
-    y_pred = model.predict(X).tolist()
-    
-    response = {
-        "predictions": y_pred,
-        "message": ["water is safe for drinking" if pred == 1 else "water is not safe for drinking" for pred in y_pred]
-    }
+    try:
+        if request.content_type == 'application/json':
+            data = request.get_json()
+            if not data:
+                return jsonify({'error': 'No data provided'}), 400
+            df = get_and_clean_data(data, is_json=True)
+        
+        elif 'multipart/form-data' in request.content_type:
+            if 'file' not in request.files:
+                return jsonify({'error': 'No file part in the request'}), 400
+            file = request.files['file']
+            if file.filename == '':
+                return jsonify({'error': 'No selected file'}), 400
+            df = get_and_clean_data(file, is_json=False)
+        
+        else:
+            return jsonify({'error': 'Unsupported media type'}), 415
 
-    return jsonify(response)
+        if 'is_safe' not in df.columns:
+            return jsonify({'error': "'is_safe' column is missing from the data."}), 400
+        
+        X_test = feature_engineering(df, is_prediction=True)
+        y_pred = model.predict(X_test).tolist()
+        
+        response = {
+            "predictions": y_pred,
+            "message": ["water is safe for drinking" if pred == 1 else "water is not safe for drinking" for pred in y_pred]
+        }
+
+        return jsonify(response)
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    # RUN_ID = 'b148d239d6b84b08961892fe7b8dfc95'
-    input_file = 'https://raw.githubusercontent.com/Tiamz01/data_repo/main/waterQuality1.csv'
-    output_file = 'output/predictions.csv'
-
-    apply_model(input_file=input_file, run_id=RUN_ID, output_file=output_file)
+    app.run(debug=True, host='0.0.0.0', port=9696)
--- deployment/batch/test.py
+++ deployment/batch/test.py
@@ -1,38 +1,44 @@
-
-from predict import apply_model
-
-input_file = 'https://raw.githubusercontent.com/Tiamz01/data_repo/main/waterQuality1.csv'
-output_file = 'output/predictions.csv'
-
-df_result = apply_model(input_file=input_file, output_file=output_file)
-print(df_result)
+import requests
 
 
-# def load_model():
-#     try:
-#        with open('best_log_reg.bin', 'rb') as f_in:
-#         model = pickle.load(f_in)
-#     except Exception as e:
-#         print("Credentials error:", e)
-#         raise
-#     return model
+# # Example input data
+# data = {
+#   "aluminium": [1.65],
+#   "ammonia": [9.08],
+#   "arsenic": [0.04],
+#   "barium": [2.85],
+#   "cadmium": [0.007],
+#   "chloramine": [0.35],
+#   "chromium": [0.83],
+#   "copper": [0.17],
+#   "flouride": [0.05],
+#   "bacteria": [0.20],
+#   "viruses": [1],
+#   "lead": [0.054],
+#   "nitrates": [1.13],
+#   "mercury": [0.007],
+#   "perchlorate": [37.75],
+#   "radium": [6.78],
+#   "selenium": [0.08],
+#   "silver": [0.34],
+#   "uranium": [0.02],
+#   "is_safe": [1]
+# }
 
-# def apply_model(input_file, output_file):
-#     df = get_and_clean_data(input_file)
-    
-#     if 'is_safe' not in df.columns:
-#         raise KeyError("'is_safe' column is missing from the data.")
-    
-#     X_train, X_test, y_train, y_test = feature_engineering(df)
-#     model = load_model()
-#     y_pred = model.predict(X_test)
-#     df_result = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
-#     # df_result['model_version'] = run_id
+# URL of the file on GitHub
+file_url = 'https://raw.githubusercontent.com/Tiamz01/data_repo/main/waterQuality1.csv'
 
-#     output_dir = os.path.dirname(output_file)
-#     if not os.path.exists(output_dir):
-#         os.makedirs(output_dir)
-#     df_result.to_csv(output_file, index=False)
+# Fetch the file
+response = requests.get(file_url)
+if response.status_code == 200:
+    files = {'file': ('waterQuality1.csv', response.content)}
+    # Upload to Flask endpoint
+    response = requests.post('http://localhost:9696/predict', files=files)
+    print(response.json())
+else:
+    print("Failed to fetch file.")
 
-#     return df_result
- 
+
+url = 'http://127.0.0.1:9696/predict'
+response = requests.post(url, json=data)
+print(response.json())
